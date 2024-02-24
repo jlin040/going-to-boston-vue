649589fcb74c6e9dc8ea897eb6d7183e5dc5b8be
jchu40 <124083963+jchu40@users.noreply.github.com>
2024-02-24 14:48:42 -0500

    created rolling dice

--- script.js
+++ script.js
@@ -1,38 +1,72 @@
-import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
 
 const app = createApp({
-    data() {
-        return {
-            playing: false,
-            maxRounds: "",
-            die: [],
-            currentRound: 1
-        };
+  data() {
+    return {
+      playing: false,
+      maxRounds: "",
+      die: [0, 0, 0],
+      currentRound: 1,
+      currentPlayer: 1,
+      p1score: 0,
+      p1rounds: 0,
+      p2score: 0,
+      p2rounds: 0,
+    };
+  },
+  methods: {
+    startGame() {
+      if (this.maxRounds === "") {
+        alert("Please set the number of rounds.");
+      } else {
+        this.playing = true;
+        this.rollDice();
+      }
     },
-    methods: {
-        startGame() {
-            if (this.maxRounds == "") alert("please set amount of rounds")
-            else {
-                this.playing = true;
-            }
-        },
-        reset() {
-            this.playing = false;
-            this.maxRounds = "";
-            this.dice1 = "";
-            this.dice2 = "";
-            this.dice3 = "";
-            this.currentRound = 1;
-        },
-        odd() {
-            if (this.maxRounds % 2 == 0) this.maxRounds++
-        },
-        rollDice() {
+    reset() {
+      this.playing = false;
+      this.maxRounds = "";
+      this.die = [0, 0, 0];
+      this.currentRound = 1;
+      this.currentPlayer = 1;
+      this.p1score = 0;
+      this.p1rounds = 0;
+      this.p2score = 0;
+      this.p2rounds = 0;
+    },
+    rollDice() {
+      this.die = [
+        Math.floor(Math.random() * 6) + 1,
+        Math.floor(Math.random() * 6) + 1,
+        Math.floor(Math.random() * 6) + 1,
+      ];
 
+
+      if (this.currentPlayer === 1) {
+        this.p1score += this.die.reduce((sum, value) => sum + value, 0);
+      } else {
+        this.p2score += this.die.reduce((sum, value) => sum + value, 0);
+      }
+
+
+      if (this.currentRound % 2 === 0) {
+        if (this.p1score > this.p2score) {
+          this.p1rounds++;
+        } else {
+          this.p2rounds++;
         }
-    }
+      }
 
-})
+      this.currentPlayer = 3 - this.currentPlayer;
 
-const vm = app.mount('#app')
-window.app = vm
+      this.currentRound++;
+
+      if (this.currentRound > this.maxRounds) {
+        this.playing = false;
+      }
+    },
+  },
+});
+
+const vm = app.mount('#app');
+window.app = vm;
